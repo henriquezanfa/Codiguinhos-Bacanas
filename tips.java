e504d85e9b2295bd4e93c24ce976b28d39d81bcb
Henrique Hadermeck <henriquezanferrari@gmail.com>
2018-12-10 13:25:04 -0200

    #2

--- tips.java
+++ tips.java
@@ -1,13 +1,31 @@
+/*
+	SPLASH SCREEN
 
+	In this case the MainActivity runs some Splash Screen 
+	for 3 seconds and then opens the HomeActivity with the
+	main code.
+*/
 
-// To animate the app initialization
+package your.pack.name;
 
-new Handler().postDelayed(new Runnable() {
-	
+import ... 
+
+public CLass MainActivity extends AppCompActivity {
+
+	// Time elapsed in the Splesh Screen
+	SPLASH_TIME = 3000; 
+
 	@Override
-	public void run() {
-		Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-		startActivity(homeIntent);
-		finish();
+	protected void onCreate(Bundle saveInstance) {
+		super.onCreate(saveInstance);
+		setContenteView(R.layout.activity_main);
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class); 
+				startActivity(homeIntent);
+				finish();
+			}
+		}, SPLASH_TIME);
 	}
-}, TIME_TO_START_NEW_ACTIVITY)
+}
