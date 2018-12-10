7356f0e0b2d2ace1463fb893c442f624691ec2c5
Henrique Hadermeck <henriquezanferrari@gmail.com>
2018-12-10 15:59:25 -0200

    #4

--- tips.java
+++ tips.java
@@ -29,3 +29,55 @@ public CLass MainActivity extends AppCompActivity {
 		}, SPLASH_TIME);
 	}
 }
+
+
+/*
+	LIST VIEW
+
+	Just putting int because everything is list view
+	It's not gonna hurt to see this again 
+*/
+
+package your.pack.name;
+
+import ... 
+
+public CLass MainActivity extends AppCompActivity {
+ 
+	@Override
+	protected void onCreate(Bundle saveInstance) {
+		super.onCreate(saveInstance);
+		
+		ListView myListView = findViewById(R.id.my_list_view);
+
+		ArrayList<String> myArray = new ArrayList<String>();
+
+		myArray.add("Item 1");
+		myArray.add("Item 2");
+		myArray.add("Item 3");
+		myArray.add("Item 4");
+
+		/*
+			Alternatively:
+			final ArrayList<String> myArray = new ArrayList<String>(asList("Item 1", "Intem 2", "Item 3", "Item 4"));
+		*/		
+
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myArray);
+		/*
+			ArrayAdapter receives 3 paremeter: context, layout resource and object to display
+			There are several different types of layouts, may worth to search for them 
+		*/
+
+		myListView.setAdapter(arrayAdapter);
+
+		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+				Toast.makeText(getApplicationContext(), myArray.get(i), Toat.LENGTH_LONG).show();
+
+			}
+		});
+	}
+}
